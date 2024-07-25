2fed780c69b9c8bc99bb844bff876d156c6a3cc5
Archana2025 <Archanap949@gmail.com>
2024-07-25 15:19:47 +0530

    update

---
+++ app_server/controllers/main.js
@@ -0,0 +1,7 @@
+module.exports.index=function(req,res){
+    res.render('index', { title: 'Eeshanth Reyhanth' });
+};
+
+module.exports.signin=function(req,res){
+    res.render('signin', { title: 'Eeshanth Reyhanth' });
+};
